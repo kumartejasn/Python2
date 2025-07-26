8d522310f6dd687dc614ac63dcfa7d94d4d03958
tejas <tejaskumarcs20@gmail.com>
2025-07-26 09:42:11 +0530

    updating

--- basics.py
+++ basics.py
@@ -1,24 +1,29 @@
-print("hello world")
-x=2
-x+=3
-print(x)
+# print("hello world")
+# x=2
+# x+=3
+# print(x)
 
-a=int(input("Enter a number:"))
-print(x+a)
-y=4
+# a=int(input("Enter a number:"))
+# print(x+a)
+# y=4
 
-# type cast y to string and concatenate with name\
+# # type cast y to string and concatenate with name\
 
-c=str(y)
-name="tejas"
-print(c)
-print(name+c)
+# c=str(y)
+# name="tejas"
+# print(c)
+# print(name+c)
 
 
-v="4"  # here v is a string
+# v="4"  # here v is a string
 
-# type cast v to integer and add to x
-c=int(v)
+# # type cast v to integer and add to x
+# c=int(v)
 
-print(x+c) # this will add the integer value of v to x
+# print(x+c) # this will add the integer value of v to x
 
+
+a=int(input("enter a intiger: "))
+b=int(input("enter another intiger: "))
+c=(a+b)/2
+print("average of two intigers is",c)
