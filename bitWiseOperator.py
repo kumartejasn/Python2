c756e55b3a804257ff050314c476c912c5766942
tejas <tejaskumarcs20@gmail.com>
2025-07-26 10:28:43 +0530

    updating

--- bitWiseOperator.py
+++ bitWiseOperator.py
@@ -2,8 +2,18 @@
 
 a=2
 b=4
+
+#AND
 print(a&b) # prints 0 because 0&0==0, 0&1==0, 1&0==0, 1&1==1
 
-
+#OR
 print(a|b)  # prints 6 because 0|0==0, 0|1==1, 1|0==1, 1&1==1
 
+#XOR
+print(a^b)  #print 6 because 0^0==0, 0^1==1, 1^0==1, 1^1==0 becausein XOR the values must be different and if they are same it wilol return 0
+
+#left shift
+print(b<<2) # print 16 becasuse b<<2==b*(2**2)
+
+#right shift
+print(b>>2) # print 1 because b>>2==b/(2**2)
