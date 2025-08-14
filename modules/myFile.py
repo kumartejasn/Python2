f1e2a40062712053bfa1bbe3195fb6ddbce06f93
tejas <tejaskumarcs20@gmail.com>
2025-08-14 19:57:23 +0530

    modules

--- modules/myFile.py
+++ modules/myFile.py
@@ -1,3 +1,15 @@
-import modules
-n=int(input("Enter the number to find fibonacci: "))
-print(modules.fib(n))
+# import modules
+# n=int(input("Enter the number to find fibonacci: "))
+# print(modules.fib(n))
+
+
+# import random
+# otp=random.randint(10000,99999)
+# print(otp)
+
+import math
+a=int(input("enter the value of a: "))
+b=int(input("enter the value of b: "))
+sum=a + b
+sr =  math.sqrt(sum)
+print(sr)
